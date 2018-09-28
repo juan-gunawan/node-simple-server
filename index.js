3b05ba6ddc90fbee0370ba5f0c9ed396e5d9a329
Juan Gunawan <juan.gunawan@traveloka.com>
2018-09-28 16:53:41 +0700

    Header

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Mock JSON
-const oneway = require('./mocks/oneway');
+const mock = require('./mocks/oneway');
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/oneway', (req, res) => {
-  res.send(oneway);
+app.get('/mock', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(mock);
 });
 
 app.listen(port, () => console.log(`Simple Server listening on port ${port}!`));
